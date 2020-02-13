c2b2b33ea77601b377b9dde9f65694bd0e4840be
Noah Rix <34725075+NoahRix@users.noreply.github.com>
2020-02-12 21:04:08 -0600

    Code Cleanup

--- GameLayer.cs
+++ GameLayer.cs
@@ -1,4 +1,9 @@
-﻿using System;
+/*******************************************************
+ * Programmer: Noah Rix
+ * Date Created: November 1, 2019
+ *******************************************************/
+
+using System;
 using System.Collections.Generic;
 using CocosSharp;
 using Android.Content;
@@ -6,117 +11,130 @@ using Android.Media;
 using Android.Gms.Ads;
 namespace Swipe_Pipe
 {
-    public class GameLayer : CCLayerColor
+    public class GameLayer : CCLayer
     {
-        MainActivity context;
-        Random random = new Random();
-        Background background;
-        Pipes pipes;
-        List<Bird> birds = new List<Bird>();
-        GameOver gameover;
-        CCLabel score_front;
-        CCLabel score_shadow;
-        MediaPlayer player_1;
-        MediaPlayer player_2;
-        public int width;
-        public int height;
-        public int game_state = 0;
-        public int ad_count = 0;
-        int sound_coin_id = Resource.Raw.coin;
-        int hitmark_coin_id = Resource.Raw.hitmark;
-        int score_count = 0;
-        int main_game_tick = 0;
-        int post_game_tick = 0;
-        float bird_min;
-        float bird_max;
-        float pipes_y;
-        float random_number;
-        float touch_delta = 0;
-        bool game_reset_flag;
-        public GameLayer(MainActivity _context) : base(CCColor4B.Black)
-        {
-            context = _context;
-            width = context.Resources.DisplayMetrics.WidthPixels;
-            height = context.Resources.DisplayMetrics.HeightPixels;
-            player_1 = MediaPlayer.Create(context, sound_coin_id);
-            player_2 = MediaPlayer.Create(context, hitmark_coin_id);
-            gameover = new GameOver(this);
-            pipes = new Pipes(this, context);
-            background = new Background(this, pipes);
-            for (int i = 0; i < 1; i++)
-                birds.Add(new Bird(this));
-            score_front = new CCLabel("0", "Fonts/04B_19__.TTF", height * 0.15f);
-            score_shadow = new CCLabel("0", "Fonts/04B_19__.TTF", height * 0.15f);
+        Random random = new Random();                   //For random number generation.
+        Background background;                          //Background nodes.
+        Pipes pipes;                                    //Pipe nodes.
+        List<Bird> birds = new List<Bird>();            //List of birds.
+        ActionMenu actionMenu;                          //Start and gameover action menus.
+        CCLabel scoreFront;                             //Score counter.
+        CCLabel scoreShadow;                            //Score counter shadow. (just another text layer but black font color.)
+        MediaPlayer player_1;                           //For sound effect.
+        MediaPlayer player_2;                           //For sound effect.
+        public int width;                               //Absoute screen width by pixel.
+        public int height;                              //Absoute screen height by pixel.    
+        public int gameState = 0;                       //Value for game state. Pre, main, and, post.
+        public int adCount = 0;                         //How many gameovers per one interstitial ad.
+        int soundCoinId = Resource.Raw.coin;            //Coin sound effect.
+        int hitmarkCoinId = Resource.Raw.hitmark;       //Hitmark sound effect.
+        int scoreCount = 0;                             //Score counter value.
+        int mainGameTick = 0;                           //Main game tick for time tracking.
+        int postGameTick = 0;                           //Post game tick for time tracking.
+        float birdMin;                                  //Minimum pixel height for a bird.
+        float birdMax;                                  //Maximum pixel height for a bird.
+        float pipesY;                                   //Vertical postion of the pipes in pixels.
+        float randomNumber;                             //Random number for bird replace position.
+        float touchDelta = 0;                           //Distance between two consecutive touches.
+        bool gameResetFlag;                             //If the game is ready to restart.
 
+        public GameLayer(MainActivity context)
+        {
+            width = context.screenSize.Width;
+            height = context.screenSize.Height;
+            player_1 = MediaPlayer.Create(context, soundCoinId);
+            player_2 = MediaPlayer.Create(context, hitmarkCoinId);
+            actionMenu = new ActionMenu(context.screenSize);
+            pipes = new Pipes(context.screenSize);
+            background = new Background(context.screenSize, pipes);
+            for (int i = 0; i < 1; i++)
+                birds.Add(new Bird(context.screenSize));
+            scoreFront = new CCLabel("0", "Fonts/04B_19__.TTF", height * 0.15f);
+            scoreShadow = new CCLabel("0", "Fonts/04B_19__.TTF", height * 0.15f);
+
+            //Run the game logic in a loop set to 60fps.
             Schedule(RunGameLogic);
         }
+
+        //Initiate starting values.
         void initiate()
         {
-            pipes_y = height / 2;
-            score_front.AnchorPoint = new CCPoint(0, 1.0f);
-            score_shadow.AnchorPoint = new CCPoint(0, 1.0f);
-            score_front.Position = new CCPoint(0, ContentSize.Height);
-            score_shadow.Position = new CCPoint(10, ContentSize.Height - 10);
-            score_shadow.Color = CCColor3B.Black;
-            bird_min = pipes.get_top_bounds_of_ground() + height * 0.1f;
-            bird_max = height - height * 0.1f;
-            pipes.set_position_x(ContentSize.Width - pipes.get_width());
-        }
-        void RunGameLogic(float frameTimeInSeconds)
-        {
-            switch (game_state)
-            {
-                case 0:
-                    pre_game();
-                    break;
-                case 1:
-                    main_game();
-                    break;
-                case 2:
-                    post_game();
-                    break;
-            }
-        }
-        void pre_game()
-        {
-            ad_count = 0;
-            //context.show_ad();
-            initiate();
-            for (int i = 0; i < birds.Count; i++)
-            {
-                birds[i].set_position(new CCPoint(0, ContentSize.Height / 2 + pipes.get_pipe_gap()));
-                birds[i].set_speed_x(width * 0.018f * (i + 1));
-            }
-            remove_children();
-            draw_children();
-            add_children();
-        }
-        void main_game()
-        {
-            main_game_tick += 1;
-            remove_children();
-            draw_children();
-            background.scroll_clouds();
-            bird_logic();
-            add_children();
-        }
-        void post_game()
-        {
-            post_game_tick += 1;
-            if (post_game_tick >= 30)
-                game_reset_flag = true;
-            for (int i = 0; i < birds.Count; i++)
-                birds[i].set_position(birds[i].get_position());
-            remove_children();
-            draw_children();
-            add_children();
+            pipesY = height / 2;
+            scoreFront.AnchorPoint = new CCPoint(0, 1.0f);
+            scoreShadow.AnchorPoint = new CCPoint(0, 1.0f);
+            scoreFront.Position = new CCPoint(0, ContentSize.Height);
+            scoreShadow.Position = new CCPoint(10, ContentSize.Height - 10);
+            scoreShadow.Color = CCColor3B.Black;
+            birdMin = pipes.GetTopBoundsOfGround() + height * 0.1f;
+            birdMax = height - height * 0.1f;
+            pipes.SetPositionX(ContentSize.Width - pipes.GetWidth());   
         }
 
-        void bird_logic()
+        //Based on the gamestate value, we shall know whenever the game is ready to start, has started, or is over.
+        void RunGameLogic(float frameTimeInSeconds)
+        {
+            switch (gameState)
+            {
+                case 0:
+                    PreGame();
+                    break;
+                case 1:
+                    MainGame();
+                    break;
+                case 2:
+                    PostGame();
+                    break;
+            }
+        }
+
+        //Pree game initiates starting values, clear the screen and draw the respected nodes.
+        void PreGame()
+        {
+            adCount = 0;
+            //context.show_ad();
+            initiate();
+
+            //The bird's initial position is the exact vertical mid-point of the screen plus the size of the pipe gap.
+            //The bird's speed is about 1/55ths of the width of the screen per 1/60ths of a second.
+            for (int i = 0; i < birds.Count; i++)
+            {
+                birds[i].set_position(new CCPoint(0, ContentSize.Height / 2 + pipes.GetPipeGap()));
+                birds[i].set_speed_x(width * 0.018f * (i + 1));
+            }
+            RemoveChildren();
+            DrawChildren();
+            AddChildren();
+        }
+
+        //during the main game, the background animates the clouds.
+        void MainGame()
+        {
+            mainGameTick += 1;
+            if (mainGameTick > 3600)
+                mainGameTick = 0;
+            RemoveChildren();
+            DrawChildren();
+            background.scroll_clouds();
+            BirdLogic();
+            AddChildren();
+        }
+        void PostGame()
+        {
+            postGameTick += 1;
+            if (postGameTick >= 30)
+                gameResetFlag = true;
+            for (int i = 0; i < birds.Count; i++)
+                birds[i].set_position(birds[i].get_position());
+            RemoveChildren();
+            DrawChildren();
+            AddChildren();
+        }
+
+        void BirdLogic()
         {
             for (int i = 0; i < birds.Count; i++)
             {
-                if (main_game_tick % 20 < 10)
+                if (mainGameTick % 20 < 10)
                     birds[i].set_frame(1);
                 else
                     birds[i].set_frame(2);
@@ -124,93 +142,97 @@ namespace Swipe_Pipe
                 if (birds[i].get_position().X > ContentSize.Width)
                 {
                     player_1.Start();
-                    score_count++;
-                    score_front.Text = score_count.ToString();
-                    score_shadow.Text = score_count.ToString();
-                    random_number = random.Next((int)bird_min, (int)bird_max);
-                    birds[i].set_position(new CCPoint(0, random_number));
+                    scoreCount++;
+                    scoreFront.Text = scoreCount.ToString();
+                    scoreShadow.Text = scoreCount.ToString();
+                    randomNumber = random.Next((int)birdMin, (int)birdMax);
+                    birds[i].set_position(new CCPoint(0, randomNumber));
                 }
 
                 birds[i].move();
 
-                if (pipes.is_collide(birds[i].get_sprite()))
+                if (pipes.IsCollide(birds[i].get_sprite()))
                 {
-                    game_state = 2;
+                    gameState = 2;
                     player_2.Start();
                     Unschedule(RunGameLogic);
                     Schedule(RunGameLogic);
                 }
             }
         }
-        void add_children()
+        void AddChildren()
         {
-            background.add_children();
-            pipes.add_children();
-            AddChild(score_shadow);
-            AddChild(score_front);
+            AddChild(background);
+            AddChild(pipes);
+            AddChild(scoreShadow);
+            AddChild(scoreFront);
             for (int i = 0; i < birds.Count; i++)
-                birds[i].add_children();
-            gameover.add_children();
+                AddChild(birds[i]);
+            if (gameState == 0)
+            {
+                actionMenu.AddStart();
+                AddChild(actionMenu);
+            }
+            if (gameState == 2)
+            {
+                actionMenu.AddRestart();
+                AddChild(actionMenu);
+            }
         }
-        void remove_children()
+        void RemoveChildren()
         {
-            gameover.remove_children();
-            background.remove_children();
-            pipes.remove_children();
+            RemoveChild(background);
             for (int i = 0; i < birds.Count; i++)
-                birds[i].remove_children();
-            RemoveChild(score_shadow);
-            RemoveChild(score_front);
+                RemoveChild(birds[i]);
+            RemoveChild(scoreShadow);
+            RemoveChild(scoreFront);
+            RemoveChild(pipes);
+            RemoveChild(actionMenu);
         }
-        void draw_children()
+        void DrawChildren()
         {
-            background.draw_children();
-            pipes.draw_children();
+            background.Draw();
+            pipes.Draw();
         }
         protected override void AddedToScene()
         {
             base.AddedToScene();
             var touchListener = new CCEventListenerTouchAllAtOnce();
-            touchListener.OnTouchesEnded = OnTouchesEnded;
             touchListener.OnTouchesMoved = HandleTouchesMoved;
             AddEventListener(touchListener, this);
         }
-        void OnTouchesEnded(List<CCTouch> touches, CCEvent touchEvent)
-        {
-
-        }
         void HandleTouchesMoved(List<CCTouch> touches, CCEvent touchEvent)
         {
-            touch_delta = touches[touches.Count - 1].Location.Y - touches[touches.Count - 1].PreviousLocation.Y;
+            touchDelta = touches[touches.Count - 1].Location.Y - touches[touches.Count - 1].PreviousLocation.Y;
             CCPoint loc = touches[touches.Count - 1].Location;
-            switch (game_state)
+            switch (gameState)
             {
                 case 0:
-                    game_state = 1;
+                    gameState = 1;
                     Schedule(RunGameLogic);
                     break;
                 case 1:
-                    pipes_y += touch_delta;
-                    if (pipes.at_max())
-                        pipes_y = pipes.get_max();
-                    if (pipes.at_min())
-                        pipes_y = pipes.get_min();
-                    pipes.set_position_y(pipes_y);
+                    pipesY += touchDelta;
+                    if (pipes.AtMax())
+                        pipesY = pipes.GetMax();
+                    if (pipes.AtMin())
+                        pipesY = pipes.get_min();
+                    pipes.SetPositionY(pipesY);
                     break;
-                case 2:
-                    if (game_reset_flag)
-                        if (gameover.get_button_region().ContainsPoint(touches[touches.Count - 1].Location))
-                        {
-                            game_state = 1;
-                            post_game_tick = 0;
-                            score_count = 0;
-                            pipes_y = height / 2;
-                            pipes.set_position_y(pipes_y);
-                            score_front.Text = score_count.ToString();
-                            score_shadow.Text = score_count.ToString();
+                case 2: 
+                    if (gameResetFlag)
+                        if (actionMenu.get_button_region().ContainsPoint(touches[touches.Count - 1].Location))
+                        {  
+                            gameState = 1;
+                            postGameTick = 0;
+                            scoreCount = 0;
+                            pipesY = height / 2;
+                            pipes.SetPositionY(pipesY);
+                            scoreFront.Text = scoreCount.ToString();
+                            scoreShadow.Text = scoreCount.ToString();
                             for (int i = 0; i < birds.Count; i++)
-                                birds[i].set_position(new CCPoint(0, ContentSize.Height / 2 + pipes.get_pipe_gap()));
-                            game_reset_flag = false;
+                                birds[i].set_position(new CCPoint(0, ContentSize.Height / 2 + pipes.GetPipeGap()));
+                            gameResetFlag = false;
                             Unschedule(RunGameLogic);
                             Schedule(RunGameLogic);
                         }
