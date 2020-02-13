839bebb08d3b8b93b472a4e365126ad4b058869d
Noah Rix <34725075+NoahRix@users.noreply.github.com>
2020-02-12 21:00:01 -0600

    Code Cleanup

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Android.Gms.Ads;
 using Android.App;
@@ -15,6 +15,8 @@ namespace Swipe_Pipe
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : Activity
     {
+        public CCSizeI screenSize = new CCSizeI();
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -25,15 +27,15 @@ namespace Swipe_Pipe
             Window.AddFlags(WindowManagerFlags.Fullscreen);
 
             //Initiate ad
-            string appid = "ca-app-pub-3542202851056774~5261967133";
-            string inter_ad_id = "ca-app-pub-xxxxxxxxxxxxxxxxxxxxxxxxxxx";
+            string appid = "ca-app-pub-xxxxxxxxxxxx";
+            string inter_ad_id = "ca-app-pub-3xxxxxxxxxxxxxxxxxxxxx";
             string int_ad_test = "ca-app-pub-3940256099942544/1033173712";
-            string banner_1_id = "ca-app-pub-xxxxxxxxxxxxxxxxxxxxxxxxxxx";
+            string banner_1_id = "ca-app-pub-xxxxxxxxxxxxxxxxxxxxxxxxxx";
             AdView adView1 = FindViewById<AdView>(Resource.Id.adView1);
             MobileAds.Initialize(this, appid);
             AdRequest adRequest1 = new AdRequest.Builder().Build();
             adView1.LoadAd(adRequest1);
-
+           
 
             //Get game view
             CCGameView gameView = (CCGameView)FindViewById(Resource.Id.GameView);
@@ -44,16 +46,16 @@ namespace Swipe_Pipe
         void LoadGame(object sender, EventArgs e)
         {
             CCGameView gameView = sender as CCGameView;
-            if (gameView != null)
+            if(gameView != null)
             {
                 var ContentSearchPaths = new List<string>() { "Fonts", "Sounds" };
                 CCSizeI viewSize = gameView.ViewSize;
 
-                int width = Resources.DisplayMetrics.WidthPixels;
-                int height = Resources.DisplayMetrics.HeightPixels;
+                screenSize.Width = Resources.DisplayMetrics.WidthPixels;
+                screenSize.Height = Resources.DisplayMetrics.HeightPixels;
 
                 //Set dimensions 
-                gameView.DesignResolution = new CCSizeI(width, height);
+                gameView.DesignResolution = screenSize;
 
                 gameView.ContentManager.SearchPaths = ContentSearchPaths;
 
